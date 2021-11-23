bf0f31fe576101a72c0f3ada0e09e1b03e1d5b79
richardschiemenz <richard.schiemenz@student.hpi.de>
2021-11-23 15:22:54 +0100

    added new author page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+
+    	resources :author
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   
